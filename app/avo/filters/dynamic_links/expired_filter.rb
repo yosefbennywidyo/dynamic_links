6f293db02d25095abefe1d88f9a88e496ade6b62
Yosef Benny Widyokarsono <yosefbennywidyo@gmail.com>
2025-08-09 02:32:15 +0800

    Set up a hash to map the filter values to the corresponding query objects expired_filter.rb

--- app/avo/filters/dynamic_links/expired_filter.rb
+++ app/avo/filters/dynamic_links/expired_filter.rb
@@ -1,14 +1,18 @@
 class Avo::Filters::DynamicLinks::ExpiredFilter < Avo::Filters::BooleanFilter
   self.name = "Expired"
 
+  # A hash to map the filter values to the corresponding query objects.
+  QUERY_STRATEGIES = {
+    true => ->(query) { query.where("expires_at < ?", Time.current) },
+    false => ->(query) { query.where("expires_at IS NULL OR expires_at >= ?", Time.current) }
+  }.freeze
+
   def apply(request, query, values)
     return query if values[:expired].blank?
 
-    if values[:expired]
-      query.where("expires_at < ?", Time.current)
-    else
-      query.where("expires_at IS NULL OR expires_at >= ?", Time.current)
-    end
+    query_strategy = QUERY_STRATEGIES[values[:expired]]
+
+    query_strategy.call(query)
   end
 
   def options
