69289699f57c044698b2a357a6f81ad65bb64f62
Yosef Benny Widyokarsono <yosefbennywidyo@gmail.com>
2025-08-11 02:02:35 +0800

    polymorphism

--- engines/dynamic_links/lib/dynamic_links/shortening_strategies/nano_id_strategy.rb
+++ engines/dynamic_links/lib/dynamic_links/shortening_strategies/nano_id_strategy.rb
@@ -5,6 +5,12 @@ module DynamicLinks
     # Shortens the given URL using Nano ID
     # This strategy will generate a different short URL for the same given URL
     class NanoIDStrategy < BaseStrategy
+      def initialize
+        require 'nanoid'
+      rescue LoadError
+        raise 'Missing dependency: Please add "nanoid" to your Gemfile.'
+      end
+
       # Shortens the given URL using Nano ID
       # @param url [String] The URL to shorten (not directly used in Nano ID strategy)
       # @param min_length [Integer] The size (length) of the generated Nano ID
